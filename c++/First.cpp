f82551b1f99aa7822f8566c912624155eecf3d23
shreesha-j <shreesha1005@gmail.com>
2023-10-25 23:58:06 +0530

    modified:   c++/First.cpp

--- c++/First.cpp
+++ c++/First.cpp
@@ -16,3 +16,17 @@ public:
 };
 
 // Path: leet%20code/leet_code/c%2B%2B/Second.cpp
+
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            unordered_map<int,int> m;
+            for(int i=0;i<nums.size();i++){
+                if(m.find(target-nums[i])!=m.end()){
+                    return {m[target-nums[i]],i};
+                }
+                m[nums[i]]=i;
+            }
+            return {};
+        }
+    };
